95b385b8efa18f3dba9b221120877e9fee4dd7b0
eugene <hash_ff0000@naver.com>
2025-12-25 17:23:25 +0900

    fix: 배포용 commit

--- src/main/java/com/nhnacademy/coupon/global/saga/SagaHandler.java
+++ src/main/java/com/nhnacademy/coupon/global/saga/SagaHandler.java
@@ -32,13 +32,18 @@ public class SagaHandler {
         String reason = null; // 실패시 사유
 
         try {
-            // TODO 쿠폰 사용 내역 업데이트 로직
-            // 서비스 주입받아서 하시면 됨
-
 //            testService.process(); // 일부러 재고 부족 터트리기
-
-            // 로직 중에 재고 부족하면 해당 커스텀 예외 던지시면 됩니다.
-            // 더 좋은 방법 있으면 추천 좀
+            // TODO 01 쿠폰 사용 처리 로직 (이한음 님)
+            /**
+             *  본인들 서비스 주입받아서 로직 구현하시면 됩니다.
+             *  매개변수로 넘어온 event DTO를 까보시면 필요한 정보들이 담겨 있습니다.
+             *  그거 토대로 각자 로직에 구현해주면 됨 (재고 차감, 포인트 차감, 쿠폰 사용 처리)
+             *
+             *  만약 쿠폰 사용 처리 중 오류가 발생한다?
+             *  그럼 하단에 CouponUpdateFailedException 던지면 됩니다!
+             *
+             *  더 좋은 로직 있다면 추천 가능
+             */
 
             log.error("[Coupon API] 쿠폰 사용내역 업데이트 성공 - Order : {}", event.getOrderId());
 
@@ -76,8 +81,12 @@ public class SagaHandler {
         String reason = null; // 실패시 사유
 
         try {
-            // TODO 재고 '보상' 로직
-            // 서비스 주입받아서 하시면 됨
+            // TODO 02 쿠폰 '보상' 로직 (이한음 님)
+            /**
+             * 동일하게 서비스 주입받아서 하시면 되는데,
+             * 여기서는 '뭔가 잘못돼서 다시 원복시키는 롤백'의 과정입니다.
+             * 그니까 아까 사용 처리 했던 쿠폰을 다시 원복시키는 로직을 구현하시면 됩니다.
+             */
 
             log.error("[Coupon API] 쿠폰 사용내용 복구 성공 - Order : {}", event.getOrderId());
 
